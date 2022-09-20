762b3accab1c6dfd53c505c1a34f6196e178c4c7
Masindi Cindy <59694595+MasindiCindi12@users.noreply.github.com>
2022-09-20 11:56:23 +0200

    holbertonshool system engineering devops

--- 0x14-mysql/200~1holby.sql
+++
@@ -1,3 +0,0 @@
--- Assigns new user & permissions
-CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost'
--- 0x14-mysql/3holby.sql
+++
@@ -1,4 +0,0 @@
--- Creates new user, grants replication permission, grants SELECT for mysql.user table
-CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'root';
-GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replica_user'@'%' IDENTIFIED BY 'root';
-GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
